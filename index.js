cba95f2920acf7070f709b532388f89dee1bc31d
WNHS00660\user <goto.narumi531@outlook.jp>
2023-11-07 15:46:02 +0900

    データベースへの登録など

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const http = require('http');
 const express = require('express');
 
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 //expressオブジェクトの生成
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 //mysql接続情報
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'kirumo'
+    database: 'Kirumo'
   });
 
   //mysql 接続失敗時エラー
@@ -48,14 +51,57 @@ app.get("/mysql", function(req, res){
   
 });
 
-app.post("/trying", function(req, res){
-    
-    return res.send("カード読み取り成功 IDm:");
+// 顧客情報登録
+app.post("/signup", function(req, res){
+
+  // データ受け取り
+  const gender = req.body.gender;
+  const color = req.body.color;
+  const c_size = req.body.c_size;
+  const s_size = req.body.s_size;
+  
+  connection.query(
+    // データベースに登録
+    'INSERT INTO user_info (gender,color,clothes_size,shoes_size) VALUE ("'+ gender +'","'+ color +'","'+ c_size +'","'+ s_size +'");',
+    (error, results) => {
+      console.log(results);
+      return res.send("<a href='#'>Hello World!!</a>"+results);
+    //   res.render('hello.ejs');
+    }
+  );
 });
 
-app.post("/signup", function(req, res){
+// タスク登録
+app.post("/task", function(req, res){
+
+  // データ受け取り
+  const store_id = req.body.store_id;
+  const room_id = req.body.room_id;
+  const task = req.body.task;
+
+  console.log(store_id)
+  console.log(room_id)
+  console.log(task)
+  
+  connection.query(
+    // データベースに登録
+    'INSERT INTO store_tasks (store_id,room_id,task_description) VALUE ("'+ store_id +'","'+ room_id +'",\''+ task +'\');',
+    (error, results) => {
+      console.log(results);
+      return res.send("<a href='#'>Hello World!!</a>"+results);
+    //   res.render('hello.ejs');
+    }
+  );
+});
+
+app.get("/form", function(req, res){
     
-    return res.send("新規登録 IDm:");
+      res.render('form.ejs');
+});
+
+app.get("/task", function(req, res){
+    
+  res.render('task.ejs');
 });
 
 //サーバの設定
